d82b7e7602ddfefc41efab7a2bf701c9c0c298ce
dngea <66570410+dngea@users.noreply.github.com>
2024-12-20 12:39:15 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 import {src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
+import terser from 'gulp-terser'
+import cleanCss from 'gulp-clean-css'; 
 
 const sass = gulpSass(dartSass)
-const { parallel } = require('gulp');
+
 
 export function js(done){
     src('src/js/app.js')
+        .pipe(terser())
         .pipe(dest('build/js'))
     done()
 }
@@ -14,7 +17,8 @@ export function js(done){
 export function css(done){
     src('src/sass/app.scss', {sourcemaps: true})
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('build/css', {sourcemaps: true}))
+        .pipe(cleanCss())
+        .pipe(dest('build/css', {sourcemaps: '.'}))
     done()
 }
 
